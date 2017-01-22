fdc8e7317f50fec18e779d22ce35cf8b595a84ad
nickmadden <madden.25@wright.edu>
2017-01-22 04:36:53 -0500

    Physics Update

--- Assets/Scripts/PhysicsGun2.cs
+++ Assets/Scripts/PhysicsGun2.cs
@@ -12,9 +12,10 @@ public class PhysicsGun2 : MonoBehaviour {
 	private Camera cam;
 	private float power = 0f;
 	private float distance = 0f;
+	private bool shooting = false;
+	private float dir = 0.0f;
 	private float[] rayGridY = new float[] {0.60f, 0.59f, 0.58f, 0.57f, 0.56f, 0.55f, 0.54f, 0.53f, 0.52f, 0.51f, 0.50f, 0.49f, 0.48f, 0.47f, 0.46f, 0.45f, 0.44f, 0.43f, 0.42f, 0.41f, 0.40f};
 	private float[] rayGridX = new float[] {0.43f, 0.45f, 0.47f, 0.48f, 0.49f, 0.50f, 0.51f, 0.52f, 0.53f, 0.55f, 0.57f};
-
 	string pushAxis;
 	string pullAxis;
 
@@ -48,63 +49,71 @@ public class PhysicsGun2 : MonoBehaviour {
 			power += charging * maxPower / chargeFrames;
 			distance += maxDistance / chargeFrames;
 		}
-		else if ((Input.GetAxis (pushAxis) == 0 || Input.GetButtonUp (pushAxis) || Input.GetAxis (pullAxis) == 0 || Input.GetButtonUp (pullAxis)) && charging != 0) {
-			//Debug.Log ("Go");
-			VisionCheck();
-				charging = 0;
-				power = 0;
+		if (((Input.GetAxis (pushAxis) == 0 || Input.GetButtonUp (pushAxis)) && charging > 0) || ((Input.GetAxis (pullAxis) == 0 || Input.GetButtonUp (pullAxis)) && charging < 0)) {
+			shooting = true;
+			dir = charging;
+			charging = 0;
 		}
-		else {
+	}
 
+	void FixedUpdate() {
+		if (shooting) {
+			VisionCheck ();
+			shooting = false;
+			power = 0;
+			distance = 0;
 		}
 	}
 
 	void VisionCheck(){
-		if (charging > 0) {
+		if (dir > 0) {
 			List<Rigidbody> bodies = new List<Rigidbody> ();
-			List<Vector3> directions = new List<Vector3> ();
 			foreach (float y in rayGridY) {
 				foreach (float x in rayGridX) {
 					Ray ray = cam.ViewportPointToRay (new Vector3 (x, y, 0));
-
 					RaycastHit hit;
-					if (Physics.Raycast (ray, out hit) && hit.distance < distance) {
+					if (Physics.Raycast (ray, out hit, distance)) {
+						//LineRenderer lr;
+						//var lineObject = new GameObject ("Line");
+						//lr = lineObject.AddComponent<LineRenderer> ();
+						//lr.material = new Material (Shader.Find ("Particles/Additive"));
+						//lr.startColor = Color.red;
+						//lr.startWidth = 0.2f;
+						//lr.numPositions = 2;
+						//lr.SetPosition (0, ray.origin);
+						//lr.SetPosition (1, hit.point);
 						Rigidbody rb = hit.collider.GetComponent<Rigidbody> ();
-						if (rb && !bodies.Contains (rb)) {
+						if (rb && !bodies.Contains(rb)) {
+							float hitDist = Vector3.Distance (ray.origin, hit.point);
 							bodies.Add (rb);
-							directions.Add (ray.direction);
-
+							rb.AddForce (ray.direction.normalized * power);
+							if (hitDist < 10) {
+								Rigidbody p = GetComponentInParent<Rigidbody> ();
+								p.AddForce (ray.direction.normalized * -0.5f * power);
+							}
 						}
 					}
 				}
 			}
-			int i = 0;
-			foreach (Rigidbody rb in bodies) {
-				float hitDist = Vector3.Distance (rb.transform.position, transform.position);
-				rb.AddForce (6 * directions [i] * power / hitDist, ForceMode.VelocityChange);
-				if (hitDist < 10) {
-					Rigidbody p = GetComponentInParent<Rigidbody> ();
-					p.AddForce (directions [i] * -1 * power / (hitDist * 2), ForceMode.VelocityChange);
-				}
-			}
+			bodies.Clear ();
 		} else {
 			Ray ray = cam.ViewportPointToRay (new Vector3 (.5f, .5f, 0));
 			RaycastHit hit;
-			if (Physics.Raycast (ray, out hit) && hit.distance < (distance*distance)) {
+			if (Physics.Raycast (ray, out hit, distance*distance)) {
 				Rigidbody p = GetComponentInParent<Rigidbody> ();
-				p.AddForce (ray.direction * power * -1 / 20, ForceMode.VelocityChange);
+				p.AddForce (ray.direction.normalized * power * -1);
 			}
 		}
 
 	}
 
 	void DebugVision() {
-		foreach (float y in rayGridY) {
-			foreach (float x in rayGridX) {
-				Ray ray = cam.ViewportPointToRay (new Vector3 (x, y, 0));
-				Debug.DrawRay (ray.origin, ray.direction * distance);
-					}
+			foreach (float y in rayGridY) {
+				foreach (float x in rayGridX) {
+					Ray ray = cam.ViewportPointToRay (new Vector3 (x, y, 0));
+				Debug.DrawRay (ray.origin, ray.direction * maxDistance);
 				}
 			}
+	}
 
 }
